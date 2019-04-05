2db8bcfee78dae5ed7d8766c73ee568b9a1266a0
Uchenna Edeh <write2uche@gmail.com>
2019-04-05 17:31:29 -0500

    added merging 2 list solution. Modifield remove even element solution...

---
+++ merge_two_sorted_lists.py
@@ -0,0 +1,60 @@
+"""
+Challenge 2: Merge Two Sorted Lists
+Given two sorted lists, merge them into one list which should also be sorted. Implement the solution in Python and see if your code runs successfully!
+Input:
+Two sorted lists.
+
+Output:
+A merged and sorted list consisting of all elements of both input lists.
+
+Sample Input
+arr1 = [1,3,4,5]  
+arr2 = [2,6,7,8]
+Sample Output
+arr = [1,2,3,4,5,6,7,8]
+"""
+
+def merge_two_sorted_lists(list_a, list_b):
+    i = 0
+    j = 0
+    
+    new_list = []
+    l = len(list_a) + len(list_b)
+
+    while(i + j < l):
+        print(i, j)
+        if i > len(list_a) - 1:
+            new_list.append(list_b[j])
+            j = j + 1
+            continue
+
+        if j > len(list_b) - 1:
+            new_list.append(list_a[j])
+            i = i + 1
+            continue
+            
+        if list_a[i] < list_b[j]:
+            new_list.append(list_a[i])
+            i = i + 1
+        elif list_b[j] < list_a[i]:
+            new_list.append(list_b[j])
+            j = j + 1
+        else: # must be equal
+            new_list.append(list_a[i])
+            new_list.append(list_b[j])
+            i = i + 1
+            j = j + 1
+
+    return new_list
+        
+arr1 = [1,3,4,5]  
+arr2 = [2,6,7,8]
+print(merge_two_sorted_lists(arr1, arr2))
+
+arr1 = [1,3,3,3]  
+arr2 = [2,3,3,3]
+print(merge_two_sorted_lists(arr1, arr2))
+
+
+
+
--- remove_even_integers_from_list.py
+++ remove_even_integers_from_list.py
@@ -1,15 +1,40 @@
 """
 Challenge 1: Remove Even Integers from List
 Given an array of size n, remove all even integers from it. Implement this solution in Python and see if it runs without an error.
+Input
+A list with random integers.
+
+Output
+A list with only odd integers
+
+Sample Input
+myList = [1,2,4,5,10,6,3]
+Sample Output
+myList = [1,5,3]
 """
+import array
 
 def remove_even_int(my_list):
+    """ Constant space solution """
     length_list = len(my_list)
-    for i in range(len(my_list)):
-        if i < length_list and my_list[i] %  2 == 0:            
+    i = 0
+    # for i in range(len(my_list)):
+    while(i < length_list):
+        if my_list[i] %  2 == 0:            
             del my_list[i]
+        else:
+            i = i + 1
         length_list = len(my_list)
+        # print(my_list)
 
+    # return [i for i in range(length_list) if my_list[i] % 2 == 0] # solution with list comprehension
     return my_list
 
-print(remove_even_int([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+my_list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+my_list2 = [1, 2, 4, 5, 10, 6, 3]
+
+print(remove_even_int(my_list1))
+print(remove_even_int(my_list2))
+
+print(remove_even_int(array.array('i', my_list1)))
+print(remove_even_int(array.array('i', my_list2)))
